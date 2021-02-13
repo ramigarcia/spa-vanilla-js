7f003e83316c10232fcd3f6604eeefdda501c05f
Ramiro Garcia <ramirogarciaweb@gmail.com>
2021-02-12 21:52:04 -0300

    [Add] Config

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HTMLWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: __dirname + '/dist',
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/i,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: [
+          'style-loader',
+          'css-loader'
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|webp)$/i,
+        use: ["file-loader?name=assets/[name].[ext]"],
+      },
+      {
+        test: /\.(woff)$/i,
+        use: ["file-loader?name=assets/[name].[ext]"],
+      },
+    ],
+  },
+  plugins: [
+    new HTMLWebpackPlugin({
+      template: "./src/index.html"
+    }),
+  ],
+}
